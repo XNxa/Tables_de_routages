aa825b9d549ef6ea29715bcb733f048e749eceb5
GPL <gaetanpoupart@gmail.com>
2023-01-05 19:07:29 +0100

    Test  adresse ip

--- test_adresse_ip.adb
+++ test_adresse_ip.adb
@@ -14,6 +14,7 @@ procedure Test_Adresse_IP is
     Adresse_String : Unbounded_String;
     Octet1, Octet2, Octet3, Octet4 : Integer;
     Fichier : File_Type;
+    SupMasque : Boolean;
 
     procedure Test_de_la_fonction_Initialiser is
     begin
@@ -23,7 +24,7 @@ procedure Test_Adresse_IP is
         Octet4 := 1;
         Initialiser(Adresse1, Octet1, Octet2, Octet3, Octet4);
         Adresse_String := To_UString_Base10(Adresse1);
-        Put_Line("Test de la fonction Initialiser : " & Adresse_String);
+        Put_Line("Test de la fonction Initialiser avec octets quelconques : " & Adresse_String);
         pragma Assert (Adresse_String = "192.168.0.1");
 
         Octet1 := 0;
@@ -57,54 +58,120 @@ procedure Test_Adresse_IP is
 
         Lire_Adresse(Adresse1, Fichier);
         Adresse_String := To_UString_Base10(Adresse1);
-        Put_Line("Test avec une adresse IP invalide dans le fichier : " & Adresse_String);
-        Close(Fichier);
+        Put_Line("Test avec une adresse IP trop longue dans le fichier : " & Adresse_String);
 
-        -- Créer un fichier vide "adresse.txt"
-        Open(Fichier, In_File, "test_adresse_vide.txt");
         Lire_Adresse(Adresse1, Fichier);
         Adresse_String := To_UString_Base10(Adresse1);
-        Put_Line("Test avec un fichier vide : " & Adresse_String);
+        Put_Line("Test avec une adresse IP trop courte dans le fichier : " & Adresse_String);
         Close(Fichier);
 
-         Put_Line("Fonction Lire_Adresse OK");
+        Put_Line("Fonction Lire_Adresse OK");
     end Test_de_la_fonction_Lire_Adresse;
 
-begin
--- 
-     Octet1 := 0;
+    procedure Test_To_UString_Base10 is
+    begin
+        Octet1 := 192;
+        Octet2 := 168;
+        Octet3 := 0;
+        Octet4 := 1;
+        Initialiser(Adresse1, Octet1, Octet2, Octet3, Octet4);
+        Adresse_String := To_UString_Base10(Adresse1);
+        Put_Line("Test Test_To_UString_Base10 avec octets quelconques : " & Adresse_String);
+        pragma Assert (Adresse_String = "192.168.0.1");
+
+        Put_Line("Fonction To_UString_Base10 OK");
+    end Test_To_UString_Base10;
+
+    procedure Test_Est_Compatible is
+    begin
+        Octet1 := 168;
+        Octet2 := 97;
+        Octet3 := 0;
+        Octet4 := 0;
+        Initialiser(Adresse1, Octet1, Octet2, Octet3, Octet4);
+        Octet1 := 255;
+        Octet2 := 255;
+        Octet3 := 255;
+        Octet4 := 255;
+        Initialiser(Masque, Octet1, Octet2, Octet3, Octet4);
+        Octet1 := 168;
+        Octet2 := 97;
+        Octet3 := 18;
+        Octet4 := 22;
+        Initialiser(Adresse2, Octet1, Octet2, Octet3, Octet4);
+        Compatible := Est_Compatible(Adresse2, Masque, Adresse1);
+        Put_Line("Test avec deux adresses partiellement différentes et un masque long : " & Boolean'Image(Compatible));
+
+        Octet1 := 168;
+        Octet2 := 97;
+        Octet3 := 0;
+        Octet4 := 0;
+        Initialiser(Adresse1, Octet1, Octet2, Octet3, Octet4);
+        Octet1 := 255;
+        Octet2 := 255;
+        Octet3 := 0;
+        Octet4 := 0;
+        Initialiser(Masque, Octet1, Octet2, Octet3, Octet4);
+        Octet1 := 168;
+        Octet2 := 97;
+        Octet3 := 18;
+        Octet4 := 22;
+        Initialiser(Adresse2, Octet1, Octet2, Octet3, Octet4);
+        Compatible := Est_Compatible(Adresse2, Masque, Adresse1);
+        Put_Line("Test avec deux adresses partiellement différentes et un masque court : " & Boolean'Image(Compatible));
+
+        Put_Line("Fonction Est_Compatible OK");
+    end Test_Est_Compatible;
+
+    procedure Test_Sup_Masque is
+    begin
+        Octet1 := 0;
         Octet2 := 0;
         Octet3 := 0;
         Octet4 := 0;
         Initialiser(Adresse1, Octet1, Octet2, Octet3, Octet4);
-        Initialiser(Masque, Octet1, Octet2, Octet3, Octet4);
+        Initialiser(Adresse2, Octet1, Octet2, Octet3, Octet4);
+        SupMasque := Sup_Masque(Adresse1, Adresse2);
+        Put_Line("Test avec deux adresses égales : " & Boolean'Image(SupMasque));
+
         Octet1 := 0;
         Octet2 := 0;
         Octet3 := 0;
         Octet4 := 1;
-        Initialiser(Adresse3, Octet1, Octet2, Octet3, Octet4);
-        Compatible := Est_Compatible(Adresse1, Masque, Adresse3);
-        Put_Line("Test de la fonction Est_Compatible avec une adresse et un masque de valeur minimale : " & Boolean'Image(Compatible));
+        Initialiser(Adresse1, Octet1, Octet2, Octet3, Octet4);
+        Octet4 := 0;
+        Initialiser(Adresse2, Octet1, Octet2, Octet3, Octet4);
+        SupMasque := Sup_Masque(Adresse1, Adresse2);
+        Put_Line("Test avec Adresse de droite supérieure à Adresse de gauche : " & Boolean'Image(SupMasque));
 
+        Octet1 := 0;
+        Octet2 := 0;
+        Octet3 := 0;
+        Octet4 := 1;
+        Initialiser(Adresse1, Octet1, Octet2, Octet3, Octet4);
+        Octet4 := 0;
+        Initialiser(Adresse2, Octet1, Octet2, Octet3, Octet4);
+        SupMasque := Sup_Masque(Adresse2, Adresse1);
+        Put_Line("Test avec Adresse de gauche supérieure à Adresse de droite : " & Boolean'Image(SupMasque));
 
+        Put_Line("Fonction Sup_Masque OK");
+    end Test_Sup_Masque;
 
+begin
 
-    -- Test de la fonction To_UString_Base10
-    UString := To_UString_Base10(Adresse2);
-    Put_Line("Test de la fonction To_UString_Base10 : " & UString);
-
-    -- Test de la fonction Est_Compatible
-    Octet1 := 255;
-    Octet2 := 255;
-    Octet3 := 0;
-    Octet4 := 0;
-    Initialiser(Masque, Octet1, Octet2, Octet3, Octet4);
-    Octet1 := 192;
-    Octet2 := 168;
-    Octet3 := 0;
-    Octet4 := 2;
-    Initialiser(Adresse3, Octet1, Octet2, Octet3, Octet4);
-    Compatible := Est_Compatible(Adresse1, Masque, Adresse3);
-    Put_Line("Test de la fonction Est_Compatible : " & Boolean'Image(Compatible));
+    Put_Line("Test de la fonction Initialiser :");
+    Test_de_la_fonction_Initialiser;
+    new_Line;
+    --Put_Line("Test de la fonction Lire_Adresse :");
+   -- Test_de_la_fonction_Lire_Adresse;
+    new_Line;
+    Put_Line("Test de la fonction Est_Compatible :");
+    Test_Est_Compatible;
+    new_Line;
+    Put_Line("Test de la fonction To_UString_Base10 :");
+    Test_To_UString_Base10;
+    new_Line;
+    Put_Line("Test de la fonction Sup_Masque :");
+    Test_Sup_Masque;
 
 end Test_Adresse_IP;
